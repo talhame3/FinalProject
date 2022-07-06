872f05b7d11a53beb53a53b574b59de819fe7c84
Talha Hassan <1000073709@hexaware.com>
2022-07-06 12:08:41 -0400

    Final Project

---
+++ src/main/java/com/hexaware/spring/ClaimManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hexaware.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimManagementSystemApplication.class, args);
+	}
+
+}
